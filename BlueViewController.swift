314eed0daff7f0df8be7ee2a7131c87008761fb1
zhoujun0920 <zhoujun199009@hotmail.com>
2015-06-23 22:16:01 -0500

    first commit

---
+++ BlueViewController.swift
@@ -0,0 +1,13 @@
+//
+//  BlueViewController.swift
+//  multiview
+//
+//  Created by Jun Zhou on 6/22/15.
+//  Copyright (c) 2015 Jun Zhou. All rights reserved.
+//
+
+import Cocoa
+
+class BlueViewController: UIViewController {
+
+}
---
+++ multiview/SwitchingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SwitchingViewController.swift
+//  multiview
+//
+//  Created by Jun Zhou on 6/22/15.
+//  Copyright (c) 2015 Jun Zhou. All rights reserved.
+//
+
+import Cocoa
+
+class SwitchingViewController: UIViewController {
+
+}
